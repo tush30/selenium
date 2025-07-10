7a8507bf13ebd4bdb2adb55fe420b770b05d3b79
tush30 <105384785+tush30@users.noreply.github.com>
2025-07-10 18:21:57 +0530

    Update LoginLogout.java

--- src/main/java/org/example/LoginLogout.java
+++ src/main/java/org/example/LoginLogout.java
@@ -2,32 +2,26 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-import java.util.concurrent.TimeUnit;
-
 public class LoginLogout {
 
     @Test
-    public void testLoginAndLogout() throws InterruptedException {
+    public void loginTest() throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-
-        driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
         driver.get("https://practicetestautomation.com/practice-test-login/");
         driver.findElement(By.name("username")).sendKeys("student");
         driver.findElement(By.name("password")).sendKeys("Password123");
         driver.findElement(By.id("submit")).click();
-
         Thread.sleep(3000);
 
-        assertTrue(driver.getCurrentUrl().contains("logged-in-successfully"), "Login failed!");
+        String url = driver.getCurrentUrl();
+        assertTrue(url.contains("logged-in"), "Login failed!");
 
         driver.findElement(By.linkText("Log out")).click();
         driver.quit();
