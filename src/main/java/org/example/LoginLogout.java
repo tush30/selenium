6c3b690fb328ed166658b0f804d6ae4fa76fef04
tush30 <105384785+tush30@users.noreply.github.com>
2025-07-10 17:55:24 +0530

    Update LoginLogout.java

--- src/main/java/org/example/LoginLogout.java
+++ src/main/java/org/example/LoginLogout.java
@@ -5,9 +5,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
-
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import java.util.concurrent.TimeUnit;
-
+@Test
 public class LoginLogout {
     public static void main(String[] args) throws InterruptedException {
         //System.setProperty("webdriver.chrome.driver", "C:\\Users\\tushr\\OneDrive\\Desktop\\browserdriver\\chromedriver.exe");
