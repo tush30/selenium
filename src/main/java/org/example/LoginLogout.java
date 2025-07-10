fcdb43fac1a6e6998a13c577e83d324a9fbae52e
tush30 <105384785+tush30@users.noreply.github.com>
2025-07-10 17:32:39 +0530

    Update LoginLogout.java

--- src/main/java/org/example/LoginLogout.java
+++ src/main/java/org/example/LoginLogout.java
@@ -4,17 +4,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.util.concurrent.TimeUnit;
 
 public class LoginLogout {
     public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\tushr\\OneDrive\\Desktop\\browserdriver\\chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\tushr\\OneDrive\\Desktop\\browserdriver\\chromedriver.exe");
 //        ChromeOptions options = new ChromeOptions();
 //        options.addArguments("--no-sandbox");
 //
 //        options.addArguments("user-data-dir=C:/ChromeTemp");  // Run in headless mode if needed
-
+     WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();//maxzimize window
         driver.manage().deleteAllCookies();// delete all cookies
